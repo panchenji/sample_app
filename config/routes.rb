9b535afc92edd97d204729330e3dc2dcbf7b7c60
panchenji <panchenji@gmail.com>
2012-09-05 20:07:22 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  #get "users/new"
+
   #get "static_pages/home"
   #match '/home', :to => 'static_pages#home'
   root :to => 'static_pages#home'
@@ -8,6 +10,8 @@ SampleApp::Application.routes.draw do
   match '/about', :to => 'static_pages#about'
 
   match '/contact', :to => 'static_pages#contact'
+
+  match '/signup', :to => 'users#new'
 
   #get "static_pages/help"
 
